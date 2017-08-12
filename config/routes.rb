df7d508ffaca4e02f540777cf646913b7887fbc0
Manish <joharimani@gmail.com>
2017-08-12 22:54:48 +0530

    configure routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  namespace :student do
+    get 'home/index'
+  end
+
+  namespace :tutor do
+    get 'home/index'
+  end
+
   get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
